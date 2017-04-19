f2a323d340aea22f5d0ca8e5968c77b769bd2e63
Justin <justin.brottes@wing.eu>
2017-04-19 17:27:05 +0200

    [+] Add String to ArrayBuffer conversion Fn

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@
 * @flow
 */
 
+const rawStringToBuffer = (str: string): ArrayBuffer => {
+  let idx
+  const len = str.length
+  const arr = new Array(len)
+  for (idx = 0; idx < len; ++idx) {
+    arr[idx] = str.charCodeAt(idx) & 0xFF
+  }
+  return new Uint8Array(arr).buffer
+}
+
 const parse = (body: string, contentType: string): Object => {
 }
 console.log('Enjoy! 🍻')
